5ff72bf506ca0a5f8515d062be6ab07a7b7582fb
Shivam Mishra <coderknower@gmail.com>
2018-02-17 19:37:36 +0530

    Blog Making-Templates

--- posts/urls.py
+++ posts/urls.py
@@ -1,15 +1,16 @@
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth import authenticate,login
 
-from .views import post_home,post_create, post_detail,post_delete,post_list,post_update
+from .views import post_home,post_create, post_detail,post_delete,post_update
 
 
 urlpatterns = [
-    url(r'^home$', post_home),
+    url(r'^$', post_home),
     url(r'^create/$', post_create),
-    url(r'^detail/$', post_detail),
+    url(r'^(?P<id>\d+)/$', post_detail, name='detail'),
     url(r'^update/$', post_update),
-    url(r'^delete$', post_delete),
+    url(r'^delete/$', post_delete),
 
 ]
 
--- posts/views.py
+++ posts/views.py
@@ -1,27 +1,58 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.contrib.auth import authenticate, login
+
+from .models import Post
+from .forms import PostForm
+
+
+def post_create(request):
+    form= PostForm()
+    if request.method=='POST':
+        print(request.POST)
+    context={
+        "form":form,
+    }
+    return render(request,"post_form.html",context)
+
+
+
 
 
 def post_home(request):
-    return render(request,"home.html",{})
+    '''if request.user.is_autheticated():
+        context={
+            "title": "Home. You are logged in."
+        }
+        return render(request,"home.html",context)'''
+    queryset= Post.objects.all()
+    context = {
+            "objectlist": queryset,
+            "title": "You are not logged in"
+        }
+    return render(request,"login.html",context)
     #return HttpResponse("heyya ")
 
 
-def post_detail(request):
-    return HttpResponse("Detail ")
-
-
-def post_list(request):
-    return HttpResponse("Home")
+def post_detail(request, id=None):
+    #instance=Post.objects.get(id=2)
+    instance= get_object_or_404(Post, id=id)
+    context = {
+        "instance":instance,
+        "title": instance.title,
+    }
+    return render(request,"post_detail.html",context)
 
 
 def post_update(request):
-    return HttpResponse("update")
+    context = {
+        "title": "Update"
+    }
+    return render(request,"home.html",context)
 
 
 def post_delete(request):
     return HttpResponse("delete ")
 
-def post_create(request):
-    return HttpResponse("create ")
 
